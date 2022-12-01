02cafe0cfef16ac2e548257483eef272c07e3632
yangjinhyeok <dskzpbmk@naver.com>
2022-12-01 21:07:02 +0900

    first commit

--- 알고리즘/알고리즘 기초/401-다이나믹 프로그래밍1(연습)/9465_스티커.py
+++ 알고리즘/알고리즘 기초/401-다이나믹 프로그래밍1(연습)/9465_스티커.py
@@ -1,14 +1,15 @@
-t = int(input())
-for i in range(t):
-  s = []
-  n = int(input())
-  for k in range(2):
-    s.append(list(map(int, input().split())))
-  for j in range(1, n):
-    if j == 1:
-      s[0][j] += s[1][j - 1]
-      s[1][j] += s[0][j - 1]
-    else:
-      s[0][j] += max(s[1][j - 1], s[1][j - 2])
-      s[1][j] += max(s[0][j - 1], s[0][j - 2])
-  print(max(s[0][n - 1], s[1][n - 1]))
+test_case = int(input())
+
+for i in range(test_case):
+    numbers = []
+    n = int(input())
+    for k in range(2):
+        numbers.append(list(map(int, input().split())))
+    for j in range(1, n):
+        if j == 1:
+            numbers[0][j] += numbers[1][j - 1]
+            numbers[1][j] += numbers[0][j - 1]
+        else:
+            numbers[0][j] += max(numbers[1][j - 1], numbers[1][j - 2])
+            numbers[1][j] += max(numbers[0][j - 1], numbers[0][j - 2])
+    print(max(numbers[0][n - 1], numbers[1][n - 1]))
